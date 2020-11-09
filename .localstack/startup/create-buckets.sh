f9511b633d1252feaae615b99a848e9975497699
NuclearRedeye <j.summerfield@elifesciences.org>
2020-11-09 08:47:22 +0000

    chore: remove references to kriya

--- .localstack/startup/create-buckets.sh
+++ .localstack/startup/create-buckets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'creating buckets'
-awslocal s3 mb s3://kryia
-awslocal s3 mb s3://editor
+awslocal s3 mb s3://s3-editor-source-bucket
+awslocal s3 mb s3://s3-editor-destination-bucket
 echo 'buckets created'
--- .localstack/startup/create-sqs.sh
+++ .localstack/startup/create-sqs.sh
@@ -1,6 +1,6 @@
 echo 'creating SQS queue'
-awslocal sqs create-queue --queue-name KryiaUploadQueue
+awslocal sqs create-queue --queue-name EditorImportQueue
 echo 'SQS queue created'
 echo 'setting up bucket notifications'
-awslocal s3api put-bucket-notification-configuration --bucket kryia --notification-configuration file:///etc/localstackconf/notification.json
+awslocal s3api put-bucket-notification-configuration --bucket s3-editor-source-bucket --notification-configuration file:///etc/localstackconf/notification.json
 echo 'bucket notifications ready'
